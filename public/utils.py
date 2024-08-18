e134587e3e3f058637c6b033832e8f49bb22b6b2
teapot-4l8 <chaiyunze@gmail.com>
2024-08-18 21:29:27 +0800

    update projects

---
+++ public/utils.py
@@ -0,0 +1,11 @@
+# 使用Python的PIL库来调整图像大小
+from PIL import Image
+
+# 打开图像文件
+img = Image.open("public\纷玩岛图标.webp")
+
+# 调整图像大小为1280x720像素
+img_resized = img.resize((1280, 720))
+
+# 保存调整大小后的图像
+img_resized.save("public\纷玩岛图标_resized.webp")

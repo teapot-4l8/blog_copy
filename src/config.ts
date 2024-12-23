5b7cc20218863bc08951b11fab1a6546a9a938d7
teapot-4l8 <chaiyunze@gmail.com>
2024-12-23 13:25:00 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'CYZ个人博客';
+export const SITE_TITLE = '个人博客';
 export const SITE_DESCRIPTION = '你看不到我 :P';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
